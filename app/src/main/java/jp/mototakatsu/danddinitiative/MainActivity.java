867a4f92fb8a9eea9db4667e9b9e67385e4566cc
mototakatsu <mototakatsu@gmail.com>
2019-02-24 00:02:16 +0900

    use DataBinding

--- app/src/main/java/jp/mototakatsu/danddinitiative/MainActivity.java
+++ app/src/main/java/jp/mototakatsu/danddinitiative/MainActivity.java
@@ -1,5 +1,6 @@
 package jp.mototakatsu.danddinitiative;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,17 +10,17 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import jp.mototakatsu.danddinitiative.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setSupportActionBar(binding.toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
